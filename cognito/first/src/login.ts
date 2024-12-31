7198a0b1bf9b09f4f88e8b75f9598e62d316f742
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-12-31 12:32:43 +0200

    amplify login

--- cognito/first/src/login.ts
+++ cognito/first/src/login.ts
@@ -1,7 +1,31 @@
+import { Amplify } from "aws-amplify";
+import config from '../config.json'
+import { signIn } from '@aws-amplify/auth'
 
+Amplify.configure({
+    Auth: {
+        Cognito: {
+            userPoolId: config.amplify.userPoolId,
+            userPoolClientId: config.amplify.userPoolClientId
+        }
+    }
+})
+
+async function logIn(userName: string, password: string){
+    const signInResult = await signIn({
+        username: userName,
+        password: password
+    })
+    return signInResult;
+}
 
 async function main(){
-    console.log('hello')
+    const result = await logIn(
+        config.credentials.username,
+        config.credentials.password
+    )
+    console.log('login result:')
+    console.log(result)
 }
 
 export async function test(element: HTMLButtonElement){
