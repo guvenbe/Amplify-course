d913a2979e4668e14179910b7a456d95ee35657e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-01-22 11:29:17 +0200

    Types and operations

---
+++ data-pr/src/components/MeetingPlaces.tsx
@@ -0,0 +1,36 @@
+import type { Schema } from "../../amplify/data/resource";
+import { generateClient } from "aws-amplify/data";
+import { useEffect, useState } from "react";
+
+type MeetingPlaceType = Schema['MeetingPlace']['type']
+
+
+export function MeetingPlaces() {
+
+    const placesClient = generateClient<Schema>().models.MeetingPlace
+
+    const [meetingPlaces, setMeetingPlaces] = useState<Array<MeetingPlaceType>>([])
+
+    useEffect(() => {
+        placesClient.observeQuery().subscribe({
+            next: (data) => setMeetingPlaces([...data.items])
+        });
+    }, []);
+
+    function createPlace() {
+        placesClient.create({
+            location: window.prompt('Place location')!
+        })
+    }
+
+    return <main>
+        <button onClick={createPlace}>Create place</button>
+        <h3>All places:</h3>
+        <ul>
+            {meetingPlaces.map((place) => (
+                <li key={place.id}>{place.location}</li>
+            ))}
+        </ul>
+
+    </main>
+}
