6e28704a30e9c3647d331b912288185367d6c92b
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-01-30 10:15:50 +0200

    Project: image resize lambda

--- travel-blog/amplify/backend.ts
+++ travel-blog/amplify/backend.ts
@@ -1,7 +1,21 @@
 import { defineBackend } from '@aws-amplify/backend';
+import { imagesStorage } from './storage/resource';
+import { generateThumb } from './functions/resize/resource';
+import { EventType } from 'aws-cdk-lib/aws-s3';
+import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
 
 
-
-defineBackend({
-
+const backend = defineBackend({
+    imagesStorage,
+    generateThumb
 });
+
+backend.imagesStorage.resources.bucket.addEventNotification(
+    EventType.OBJECT_CREATED_PUT,
+    new LambdaDestination(backend.generateThumb.resources.lambda),
+    {
+        prefix: 'originals/'
+    }
+)
+
+
