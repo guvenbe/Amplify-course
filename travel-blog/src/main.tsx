81600534283b3306a0e9a64059cc70ea037bff00
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-03 11:38:28 +0200

    Auth component and checking login

--- travel-blog/src/main.tsx
+++ travel-blog/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import '@aws-amplify/ui-react/styles.css';
+import outputs from "../amplify_outputs.json";
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
