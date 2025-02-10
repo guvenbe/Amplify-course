3843eaef8f44cf46a475ff14172504e3f246a7f9
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-10 17:39:42 +0200

    add auth

--- ai/src/main.tsx
+++ ai/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Authenticator } from '@aws-amplify/ui-react'
+import outputs from '../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure(outputs);
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Authenticator>
     <App />
-  </StrictMode>,
+  </Authenticator>
+
 )
