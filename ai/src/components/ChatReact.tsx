185560edd323631fe413c21acd593806cd30bfc4
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-10 20:00:03 +0200

    Basic chat

--- ai/src/components/ChatReact.tsx
+++ ai/src/components/ChatReact.tsx
@@ -1,10 +1,23 @@
+import { AIConversation} from '@aws-amplify/ui-react-ai'
+import { useAIConversation } from '../AiClient';
 
 
-function ChatReact(){
+function ChatReact() {
 
-    return <div>
-        Hello from chat React
-    </div>
+    const chat = useAIConversation('chat')
+
+    const messages = chat[0].data.messages;
+    const sendMessage = chat[1]
+
+    return <main>
+        <h1>Hello to the awesome AI Chat!! - With Amplify React components</h1><br />
+        <AIConversation
+            messages={messages}
+            handleSendMessage={sendMessage}
+        />
+
+    </main>
 }
 
 export default ChatReact
+
