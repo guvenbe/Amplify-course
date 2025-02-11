307bf4dc561743f5ecd4a98446b32850b3afa1cd
alexhddev <21276888+alexhddev@users.noreply.github.com>
2025-02-11 10:06:23 +0200

    Functions tools

--- ai/amplify/data/resource.ts
+++ ai/amplify/data/resource.ts
@@ -1,10 +1,24 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
+import { getWeather } from '../functions/weather/resource';
 
 
 const schema = a.schema({
+  getWeather: a.query()
+  .arguments({city: a.string().required()})
+  .returns(a.customType({
+    value: a.float().required(),
+    unit: a.string().required(),
+  }))
+  .handler(a.handler.function(getWeather))
+  .authorization((allow) => allow.authenticated()),
   chat: a.conversation({
     aiModel: a.ai.model('Claude 3 Haiku'),
-    systemPrompt: 'You are helpful assistant'
+    systemPrompt: 'You are helpful assistant',
+    tools:[{
+      name: 'getWeatherTool',
+      description: 'Provide the current weather for the city',
+      query: a.ref('getWeather')
+    }]
   }).authorization(
     (allow) => allow.owner()
   )
--- ai/src/components/ChatReact.tsx
+++ ai/src/components/ChatReact.tsx
@@ -14,9 +14,44 @@ function ChatReact() {
         <AIConversation
             messages={messages}
             handleSendMessage={sendMessage}
+            responseComponents={{
+                WeatherCard: {
+                    description: 'Used to display the weather of a given city to the user',
+                    // any React component can be used
+                    component: (props) => {
+                        return <Card
+                            {...props}
+                             />;
+                    },
+                    props: {
+                        city: {
+                            type: 'string',
+                            required: true,
+                        },
+                        temperature: {
+                            type: 'string'
+                        },
+                        unit: {
+                            type: 'string'
+                        }
+                    },
+                },
+            }}
         />
 
     </main>
+}
+
+function Card(props: {
+    city: string,
+    temperature: string,
+    unit: string
+}) {
+
+    return <div style={{ border: '1px solid black' }}>
+        <h1>{props.city}</h1>
+        <h2>{props.temperature} {props.unit}</h2>
+    </div>
 }
 
 export default ChatReact
